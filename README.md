b00d05b997ddb4906e03da2b6383c2a6c89157a4
Thomas Bak <tombak98@gmail.com>
2022-12-12 11:19:15 -0500

    ready to begin

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# App Boilerplate
+# SST Coding Challenge
 
 ## Getting started
 
@@ -7,4 +7,14 @@
 3. `npm run start`
 4. name your database test
 5. Make sure you add a secrets.js file with a process.env.JWT defined
+
+# Notes to self to delete later
+## General plan for myself
+
+1. Make a HTML/CSS mockup for what it should look like
+2. Essentially, each time the timeline objects get updated, do the following
+    a. figure out whats the least and greatest time to put on the end
+    b. calculate what level each object should be on and store it in the object
+    c. the react component should take as an input what level its on (as well as start and end)
+3. then from there you can add the simple add and remove events or whatever
 
--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 const port = process.env.PORT || 3000;
 const app = require('./app');
-const db = require('./db/database')
-const seed = require('../script/seed')
+// const db = require('./db/database')
+// const seed = require('../script/seed')
 
 const init = async () => {
     try {
       //seed the database if env variable is true
-      if (process.env.SEED === "true") {
-        await seed();
-      } else {
-        await db.sync();
-      }
+      // if (process.env.SEED === "true") {
+      //   await seed();
+      // } else {
+      //   await db.sync();
+      // }
       // start listening (and create a 'server' object representing our server)
       app.listen(port, () => console.log(`Mixing it up on port ${port}`));
     } catch (ex) {
